ba8477af2d7ce0c98245880f3f02e94f0caaee2e
martinsgoser-spec <martinsgoser@gmail.com>
2025-12-05 03:45:26 +0100

    Update getorder.js

--- api/getorder.js
+++ api/getorder.js
@@ -1,3 +1,3 @@
-export default async function handler(req, res) {
-  res.status(200).json(global.orderData || { show: false });
+export default function handler(req, res) {
+  return res.status(200).json({ ok: true });
 }
